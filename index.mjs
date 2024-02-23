acc466e4a4018d9aa217e287ebab910523faf571
Graham <gleachkr@gmail.com>
2024-02-23 11:56:11 -0500

    Factor out some reasonable subcomponents, fix naming

--- index.mjs
+++ index.mjs
@@ -1,11 +1,11 @@
 import { html } from 'https://unpkg.com/htm/preact/index.module.js?module'
 import { render, Component, createRef } from 'https://unpkg.com/preact@latest?module'
-import Outlines from './components/hull.mjs'
+import Parts from './components/part.mjs'
 import { makeElfData } from './util/elfdata.mjs'
 
 class App extends Component {
 
-  constructor(props) {
+  constructor() {
     super()
     this.windowMin = 0
     this.windowMax = 5
@@ -16,24 +16,11 @@ class App extends Component {
     document.addEventListener("scroll", () => this.onScroll())
   }
 
-  async handleDrop(ev) {
-    ev.stopPropagation()
-    ev.preventDefault()
-    const file = ev.dataTransfer.files[0]
-    this.buffer = await file.arrayBuffer()
+  handleData(buffer) {
+    this.buffer = buffer
     this.view = new DataView(this.buffer)
     this.elfData = makeElfData(this.view)
     this.renderView()
-  }
-
-  handleDragover(ev) {
-    ev.stopPropagation()
-    ev.preventDefault()
-  }
-
-  handleDragleave(ev) {
-    ev.stopPropagation()
-    ev.preventDefault()
   }
 
   onScroll() {
@@ -67,31 +54,52 @@ class App extends Component {
         data-boundary=${i == 0 ? "first" : i == this.view.byteLength - 1 ? "last" : null}
         class="byte">${this.view.getUint8(i).toString(16).padStart(2, '0')}</span>`)
     }
-    this.setState({bytes}, () => {
+    this.setState({ bytes }, () => {
       if (oldOffset !== undefined) window.scroll(0, anchor.offsetTop - oldOffset + oldScroll)
     })
   }
 
-  render(props, state) {
-    if (state.bytes && this.data.current) {
+  render(_props, state) {
+    if (state.bytes) {
+      if (!this.data.current) setTimeout(() => this.forceUpdate(), 100)
       return html`<div data-state="loaded">
           <pre ref=${this.data} id="data">
           ${state.bytes}
-          <${Outlines} elfData=${this.elfData} containerElt=${this.data.current}/>
+          ${this.data.current &&
+          html`<${Parts} elfData=${this.elfData} containerElt=${this.data.current}/>`
+          }
           </pre>
         </div>`
-    } else if (state.bytes) {
-      setTimeout(() => this.forceUpdate(), 100)
-      return html`<div data-state="loaded">
-          <pre ref=${this.data} id="data">${state.bytes}</pre>
-        </div>`
     } else {
-      return html`<div data-state="awaiting"
+      return html`<${Uploader} handleData=${data => this.handleData(data)} />`
+    }
+  }
+}
+
+class Uploader extends Component {
+  async handleDrop(ev) {
+    ev.stopPropagation()
+    ev.preventDefault()
+    const file = ev.dataTransfer.files[0]
+    const buffer = await file.arrayBuffer()
+    this.props.handleData(buffer)
+  }
+
+  handleDragover(ev) {
+    ev.stopPropagation()
+    ev.preventDefault()
+  }
+
+  handleDragleave(ev) {
+    ev.stopPropagation()
+    ev.preventDefault()
+  }
+  render() {
+    return html`<div data-state="awaiting"
           onDragover=${ev => this.handleDragover(ev)}
           onDragleave=${ev => this.handleDragleave(ev)}
           onDrop=${ev => this.handleDrop(ev)}
         ><pre>awaiting file</pre></div>`
-    }
   }
 }
 
